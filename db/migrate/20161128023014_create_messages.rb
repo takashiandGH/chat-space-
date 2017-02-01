304a5e09cd484eaff28960a705d377139f4db0f0
takashi <takashiissleeping@gmail.com>
2017-02-01 19:31:37 +0900

    around migration

--- db/migrate/20161128023014_create_messages.rb
+++ db/migrate/20161128023014_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.text :body
       t.string :image
-      t.refarences :user ,null:false ,index:true ,foreign_key:true
-      t.refarences :group ,null:false ,index:true ,foreign_key:true
+      t.references :user ,null:false ,index:true ,foreign_key:true
+      t.references :group ,null:false ,index:true ,foreign_key:true
 
       t.timestamps
     end
