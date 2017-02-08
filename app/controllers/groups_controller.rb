72ba3e450fc81120bcb3b1a45321525cae1af1e7
takashi <takashiissleeping@gmail.com>
2017-02-08 19:43:39 +0900

    adding edit group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -10,12 +10,31 @@ class GroupsController < ApplicationController
     @group = Group.new(create_params)
     if @group.save
       flash[:notice] = "グループを作成しました"
-      redirect_to :root
+      redirect_to root_path
     else
       flash[:alert] = "グループを作成できませんでした"
       redirect_to new_group_path
     end
   end
+
+   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.find(params[:id])
+    group.update(create_params)
+
+    if group.update(create_params)
+      flash[:notice] = "グループ情報を更新しました"
+      redirect_to root_path
+    else
+      flash[:alert] = "グループ情報を更新できませんでした"
+      redirect_to edit_group_path
+    end
+
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
 
-  resources :groups, only: [:index, :new, :create, :update, :edit, :show]
+  resources :groups, only: [:new, :create, :update, :edit]
   resources :users,  only: [:index]
 
 end
