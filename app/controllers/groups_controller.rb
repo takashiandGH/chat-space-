58a7bf877e5345353962f33f137fa0ae3b62b862
takashi <takashiissleeping@gmail.com>
2017-02-08 19:14:54 +0900

    adding create group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(create_params)
+    if @group.save
+      flash[:notice] = "グループを作成しました"
+      redirect_to :root
+    else
+      flash[:alert] = "グループを作成できませんでした"
+      redirect_to new_group_path
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:group).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-root 'messages#index'
+  root 'messages#index'
+
+  resources :groups, only: [:index, :new, :create, :update, :edit, :show]
+  resources :users,  only: [:index]
+
 end
