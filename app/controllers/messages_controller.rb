d12333af3c728c50b129851f9a2e8b7b3e06b285
takashi <takashiissleeping@gmail.com>
2017-02-11 16:17:33 +0900

    making partial template(_sidebar,and _groups,and, _message.html.haml)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   def index
   @group = Group.find(params[:group_id])
+  @groups = current_user.groups
   @message = Message.new
   @messages = @group.messages
   end
