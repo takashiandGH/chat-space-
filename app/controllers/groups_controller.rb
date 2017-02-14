9a1d9324cda6062de0a1874651cc0fceb14cbe62
takashi <takashiissleeping@gmail.com>
2017-02-14 23:00:05 +0900

    correct redirect path

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,8 +12,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     if @group.save
       flash[:notice] = "グループを作成しました"
-      redirect_to group_messages_path
-      # ここでエラー出るのは後で修正する
+      redirect_to group_messages_path(@group.id)
     else
       flash[:alert] = "グループ名を入力してください"
       redirect_to new_group_path
