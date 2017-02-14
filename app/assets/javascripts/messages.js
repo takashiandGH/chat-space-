f5998154738242ff91e907810737652157e7f058
takashi <takashiissleeping@gmail.com>
2017-02-14 19:35:36 +0900

    copy peast from curriculums

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,38 @@
+// $(function() {
+//   $('.chat-group-form__action-btn').on('click', function(e) {
+//    e.preventDefault();
+//     message = $('.message_sent_form').val();
+//     console.log(message);
+//   });
+// });
+
 $(function() {
-  $('.chat-group-form__action-btn').on('click', function(e) {
-   e.preventDefault();
-    message = $('.message_sent_form').val();
-    console.log(message);
+  function buildHTML(todo) {
+    var html = $('<li class="todo">').append(todo.content);
+    return html;
+  }
+
+  $('.js-form').on('submit', function(e) {
+    e.preventDefault();
+    var textField = $('.js-form__text-field');
+    var todo = textField.val();
+    $.ajax({
+      type: 'POST',
+      url: '/todos.json',
+      data: {
+        todo: {
+          content: todo
+        }
+      },
+      dataType: 'json'
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.todos').append(html);
+      textField.val('');
+    })
+    .fail(function() {
+      alert('error');
+    });
   });
 });
