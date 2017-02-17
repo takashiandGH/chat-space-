a2da42ffb895061f1fea9b85c5510b097b78afe6
takashi <takashiissleeping@gmail.com>
2017-02-18 01:22:24 +0900

    deleteuserHTML

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,8 +1,8 @@
 
 $(function() {
 
-// ここはまあ大丈夫なんだと思うけど
-  function buildHTML(user) {
+//(view-A)受信したuserを表示する(追加ボタンのview)
+  function adduserHTML(user) {
     var html =
       '<div class="chat-group-user">' +
         '<span name="' + user.id + '">' +
@@ -13,7 +13,29 @@ $(function() {
     return html;
   }
 
-//userを入力、送信
+//(view-B)選択したuserを表示(削除ボタンのview)
+  function deleteuserHTML(user) {
+    var html =
+      '<div class="chat-group-user">' +
+        '<span name="' + user.id + '">' +
+          user.name +
+        '</span>' +
+        '<a class="user-search-result__btn">削除</a>' +
+      '</div>';
+    return html;
+  }
+
+
+
+
+
+
+
+
+
+
+
+//①userを入力、送信、view-Aの呼び出し
   $('#user-search-field').on('keyup', function(e) {
     e.preventDefault();
 
@@ -27,19 +49,33 @@ $(function() {
       dataType: 'json'
     })
 
-
     .done(function(data) {
-      console.log(user_name);
+      // console.log(user_name);
       $('#user-search-result').empty();
       $.each(data,function(i, user){
-        var html = buildHTML(user);
+        var html = adduserHTML(user);
           $('#user-search-result').append(html);
           });
     })
 
     .fail(function() {
-      alert('error');
+      alert('ページをリロードしてください');
     });
 
   });
+
+// ②選択したユーザーのビューを差し替え、view-Bの呼び出し(とりあえず消すところまではなんとか)
+  $('#user-search-result').on('click', '.user-search-result__btn' ,function() {
+    $(this).parent().remove()
+    var html = deleteuserHTML(user);
+    $('#user-search-result').append(html);
+
+
+  });
+
+
+
+
+
+
 });
