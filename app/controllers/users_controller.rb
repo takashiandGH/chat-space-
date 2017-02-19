00d910a0eb75a4e09abd6d889fb5edbc38996f46
takashi <takashiissleeping@gmail.com>
2017-02-19 14:26:36 +0900

    introduce jbuilder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
 
   def search
     @users = User.where(['name LIKE ?', "%#{params[:name]}%"])
-    respond_to do |format|
-      format.json { render json: @users }
+      respond_to do |format|
+      format.json
+
     end
   end
 
